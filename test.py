5152099c1543bf4a77a8474023d4503c43913543
ello <ellobudimanxt@gmail.com>
2025-02-17 00:59:10 +0700

    New: Body, Params, AVG

--- test.py
+++ test.py
@@ -5,17 +5,39 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
+list_temp=[]
+@app.route('/sensor1', methods=['POST','GET'])
+def mantap():
+    if request.method == 'POST':
+        body = request.get_json()
+        Temp = body['Temperature']
+        Hum = body['Humidity']
+        timestamp = body['timestamp']
 
-@app.route('/test', methods=['GET','POST'])
-def test():
+        #simpan data ke list sementara
+        list_temp.append({
+                "Temperature":Temp,
+                "Humidity" : Hum,
+                "Timestamp" : timestamp
+
+        })
+        return jsonify({"message" : "Processing Your request", "data" : body}),201
+    elif request.method == 'GET':
+        return jsonify(list_temp),201
+
+
+# @app.route('/sensor1', methods=['GET'])
+# def data():
+
+
+@app.route('/sensor1/temperature/avg', methods=['GET'])
+def avg():
     if request.method == 'GET':
-        return jsonify({"response": "Get Request Called"}) 
-    elif request.method == 'POST':
-        req_Json = request.json
-        name = req_Json['name']
-        return jsonify({"response":"Hi "+ name})
-
-
-
+        if not list_temp :
+            return jsonify({"average temperature" : None})
+        
+        AVG_temp = sum(entry["Temperature"] for entry in list_temp) / len(list_temp)
+        return jsonify({"Average Temp" : AVG_temp}) 
+    
 if __name__ == '__main__':
-    app.run(debug=True, port=9090)
+    app.run(debug=True, port=1280)
